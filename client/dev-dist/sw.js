89c6e16ea2ffe83d1974d763cb925cbec008a6d5
dodivar <doryandillen@gmail.com>
2024-09-17 16:30:12 +0200

    change background

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6e3354dc'], (function (workbox) { 'use strict';
     "revision": "bdbe8eee208a7580fcca7d743d73185a"
   }, {
     "url": "index.html",
-    "revision": "0.7brg60booqg"
+    "revision": "0.pu544jntn0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
